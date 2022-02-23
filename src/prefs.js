7e9bdd4dc6e0ee72f9de00945a50cbbe34d776bf
aunetx <aunetx@yandex.com>
2022-02-23 23:30:39 +0100

    Add corner color selection

--- src/prefs.js
+++ src/prefs.js
@@ -24,23 +24,28 @@ const Keys = [
 
 const Preferences = new Prefs(Keys);
 
+const parse_color_from_setting = function (setting, widget) {
+    let color_string = setting.get();
+    let is_parsed = widget.rgba.parse(color_string);
+
+    if (!is_parsed) {
+        // could not parse color, defaulting to black
+        setting.set('#000000ff');
+    }
+};
 
 var MainPage = GObject.registerClass({
     GTypeName: 'MainPage',
     Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'main_page.ui'])}`,
     InternalChildren: [
         'panel_corners',
-        'panel_corner_radius',
         'panel_corner_color',
-        'panel_corner_opacity',
         'panel_radius_adjustment',
         'panel_opacity_adjustment',
 
         'screen_corners',
-        'screen_corner_radius',
         'screen_corner_color',
-        'screen_corner_opacity',
-        'screen_opacity_adjustment',
+        'screen_radius_adjustment',
         'screen_opacity_adjustment',
 
         'force_extension_values',
@@ -54,11 +59,27 @@ var MainPage = GObject.registerClass({
         Preferences.settings.bind('panel-corners', this._panel_corners, 'state', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('panel-corner-radius', this._panel_radius_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('panel-corner-opacity', this._panel_opacity_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.PANEL_CORNER_BACKGROUND_COLOR.changed(_ => {
+            parse_color_from_setting(Preferences.PANEL_CORNER_BACKGROUND_COLOR, this._panel_corner_color);
+        });
+        this._panel_corner_color.connect('color-set', _ => {
+            let color = this._panel_corner_color.rgba.to_string();
+            Preferences.PANEL_CORNER_BACKGROUND_COLOR.set(color);
+        });
+        parse_color_from_setting(Preferences.PANEL_CORNER_BACKGROUND_COLOR, this._panel_corner_color);
 
         // Screen corners
         Preferences.settings.bind('screen-corners', this._screen_corners, 'state', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('screen-corner-radius', this._screen_radius_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('screen-corner-opacity', this._screen_opacity_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.SCREEN_CORNER_BACKGROUND_COLOR.changed(_ => {
+            parse_color_from_setting(Preferences.SCREEN_CORNER_BACKGROUND_COLOR, this._screen_corner_color);
+        });
+        this._screen_corner_color.connect('color-set', _ => {
+            let color = this._screen_corner_color.rgba.to_string();
+            Preferences.SCREEN_CORNER_BACKGROUND_COLOR.set(color);
+        });
+        parse_color_from_setting(Preferences.SCREEN_CORNER_BACKGROUND_COLOR, this._screen_corner_color);
 
         // Advanced
         Preferences.settings.bind('force-extension-values', this._force_extension_values, 'state', Gio.SettingsBindFlags.DEFAULT);
@@ -73,5 +94,5 @@ function fillPreferencesWindow(window) {
     let main_page = new MainPage();
     window.add(main_page);
     window.search_enabled = true;
-    window.set_default_size(720, 580);
+    window.set_default_size(720, 530);
 }
--- src/screen_corner.js
+++ src/screen_corner.js
@@ -185,7 +185,9 @@ let ScreenCorner = GObject.registerClass(
             super.vfunc_style_changed();
 
             let cornerRadius = Utils.lookup_for_length(null, '-screen-corner-radius', this._prefs);
+            let opacity = Utils.lookup_for_double(node, '-screen-corner-opacity', this._prefs);
 
+            this.set_opacity(opacity);
             this.set_size(cornerRadius, cornerRadius);
             this._update_allocation();
         }
