3f7d52f1f7a43260571ee2490ab81fb56a19b1ea
Aurélien Hamy <aunetx@yandex.com>
2024-04-09 23:58:59 +0200

    Fix that bug here tooo!!!

--- src/conveniences/connections.js
+++ src/conveniences/connections.js
@@ -1,22 +1,39 @@
 import GObject from 'gi://GObject';
 
-/** object to easily manage signals. */
+/// An object to easily manage signals.
 export class Connections {
     constructor() {
         this.buffer = [];
     }
 
-    /**
-     * Process the given actor and id.
-     *
-     * This makes sure that the signal is disconnected when the actor is
-     * destroyed, and that the signal can be managed through other Connections
-     * methods.
-     */
+    /// Adds a connection.
+    ///
+    /// Takes as arguments:
+    /// - an actor, which fires the signal
+    /// - signal(s) (string or array of strings), which are watched for
+    /// - a callback, which is called when the signal is fired
+    connect(actor, signals, handler) {
+        if (signals instanceof Array) {
+            signals.forEach(signal => {
+                let id = actor.connect(signal, handler);
+                this.process_connection(actor, id);
+            });
+        } else {
+            let id = actor.connect(signals, handler);
+            this.process_connection(actor, id);
+        }
+
+    }
+
+    /// Process the given actor and id.
+    ///
+    /// This makes sure that the signal is disconnected when the actor is
+    /// destroyed, and that the signal can be managed through other Connections
+    /// methods.
     process_connection(actor, id) {
         let infos = {
-            actor,
-            id
+            actor: actor,
+            id: id
         };
 
         // remove the signal when the actor is destroyed
@@ -36,39 +53,26 @@ export class Connections {
                     this.buffer.splice(index, 1);
                 }
             });
+            infos.destroy_id = destroy_id;
         }
 
         this.buffer.push(infos);
     }
 
-    /**
-     * Adds a connection.
-     *
-     * Takes as arguments:
-     * - an actor, which fires the signal
-     * - a signal (string), which is watched for
-     * - a callback, which is called when the signal is fired
-     */
-    connect(actor, signal, handler) {
-        let id = actor.connect(signal, handler);
-
-        this.process_connection(actor, id);
-    }
-
-    /**
-     * Disconnects every connection found for an actor.
-     */
+    /// Disconnects every connection found for an actor.
     disconnect_all_for(actor) {
         // get every connection stored for the actor
         let actor_connections = this.buffer.filter(
-            infos => infos.actor == actor
+            infos => infos.actor === actor
         );
 
         // remove each of them
-        actor_connections.forEach((connection) => {
+        actor_connections.forEach(connection => {
             // disconnect
             try {
                 connection.actor.disconnect(connection.id);
+                if ('destroy_id' in connection)
+                    connection.actor.disconnect(connection.destroy_id);
             } catch (e) {
                 this.#warn(`error removing connection: ${e}; continuing`);
             }
@@ -79,12 +83,14 @@ export class Connections {
         });
     }
 
-    /** Disconnect every connection for each actor. */
+    /// Disconnect every connection for each actor.
     disconnect_all() {
-        this.buffer.forEach((connection) => {
+        this.buffer.forEach(connection => {
             // disconnect
             try {
                 connection.actor.disconnect(connection.id);
+                if ('destroy_id' in connection)
+                    connection.actor.disconnect(connection.destroy_id);
             } catch (e) {
                 this.#warn(`error removing connection: ${e}; continuing`);
             }
